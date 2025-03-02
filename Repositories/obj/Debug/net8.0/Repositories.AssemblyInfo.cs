baf7e9dfae83203d8b912eb7da855daa8e5a9c1b
harsh6754 <agarwalharsh6583@gmail.com>
2025-03-02 17:37:32 +0530

    Student Dashboard Design Done

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446439576d63a9d5fbd0a1e7de146376eef14fc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71e6bb329dd56c59fc60ba3d861659687c3aeec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ StudentManagementSystem/Controllers/StudentDashboardController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentDashboardController : Controller
+    {
+        private readonly ILogger<StudentDashboardController> _logger;
+
+        public StudentDashboardController(ILogger<StudentDashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TimeTable()
+        {
+            return View();
+        }
+
+        public IActionResult UpcomingExam(){
+            return View();
+        }
+
+        public IActionResult TeacherRating(){
+            return View();
+        }
+
+        public IActionResult SyllabusProgress(){
+            return View();
+        }
+
+        public IActionResult Logout(){
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
