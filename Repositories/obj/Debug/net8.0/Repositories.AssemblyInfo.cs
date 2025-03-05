ad7ce5c50cb2521daaad758cc1ab0ca7393927eb
harsh6754 <agarwalharsh6583@gmail.com>
2025-03-05 10:22:53 +0530

    Update Admin Dashboard

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cbff5acb582effc06b7fb929bb34f68c07601f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23235ca5e8f85e7d349794c90927ad8ed9a1ffe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
