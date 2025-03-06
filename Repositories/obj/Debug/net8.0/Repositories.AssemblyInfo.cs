d36309002fd6b79a8d8dd4cda94f0983c237da8f
harsh6754 <agarwalharsh6583@gmail.com>
2025-03-06 08:53:11 +0530

    Update Admin Home Page

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be8b49d9db6154a68c85090d28b00330bd03509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac748e5f31a6f5fdbd223eec3b358bae023a57e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
