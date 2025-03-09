9dab331d1b552992ce2098035196c2bd7f5e7a24
harsh6754 <agarwalharsh6583@gmail.com>
2025-03-09 11:47:21 +0530

    Add A Feature Until Student Not Verify Account Not Able To Access The Page

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac748e5f31a6f5fdbd223eec3b358bae023a57e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36309002fd6b79a8d8dd4cda94f0983c237da8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
